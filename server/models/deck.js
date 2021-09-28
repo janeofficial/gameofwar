aa00a614c32b9a4b8a930b2d3e5413d07df0370d
Jane Balfus <janebalfus@gmail.com>
2021-09-28 11:39:48 -0400

    Description on deck methods

--- server/models/deck.js
+++ server/models/deck.js
@@ -4,7 +4,7 @@ export default class Deck {
   }
 
   generate() {
-   // Generate a deck of Cards - deck has 4 suits, each suit has 13 cards
+   // Generates a deck of Cards - deck has 4 suits, each suit has 13 cards
    let suitArr = ["Heart", "Club", "Spade", "Diamond"];
    let cardArr = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
@@ -12,10 +12,10 @@ export default class Deck {
   }
 
   randomCardIdx() {
-    return Math.floor(Math.random() * (this.cards.length));
+    return Math.floor(Math.random() * (this.cards.length)); // picking a random num between 0 and length of the deck
   }
   draw() {
-    return this.cards.splice(this.randomCardIdx(), 1);
+    return this.cards.splice(this.randomCardIdx(), 1); // -> removes Card from this.deck and returns [Card]
   }
 }
 
