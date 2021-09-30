b0462b4ef9a0daed9c1a64f0c3989ae2649507ef
Jane Balfus <janebalfus@gmail.com>
2021-09-30 16:10:11 -0400

    Save wins to db

--- server/routes/games.js
+++ server/routes/games.js
@@ -10,17 +10,19 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
   const wargame = new WarGame()
   wargame.startGame()
+  if (wargame.winner) {
+    const winnerId = wargame.winner.id
+    await client.query(`INSERT INTO wins (player_id) VALUES (${winnerId})`)
+  }
   res.json({
-    wargame,
     message: wargame.handleWin()
   })
 })
 
 router.get('/wins', async function(req, res, next) {
-  await client.connect()
   const { rows } = await client.query(`SELECT count(*), player_id from wins GROUP BY player_id;`)
   res.json(rows)
 })
